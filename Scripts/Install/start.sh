d7d083f0dc6ce4c134e5d418b074f5d5186a56b3
aliammar03 <93523392+aliammar03@users.noreply.github.com>
2023-05-28 01:21:29 +0500

    Create start.sh

---
+++ Scripts/Install/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Update Time
+timedatectl set-timezone Asia/Karachi
+
+# Reflector
+reflector --verbose --latest 20 --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+
+# Backup the original Pacman configuration file
+cp /etc/pacman.conf /etc/pacman.conf.bak
+
+# Enable parallel downloads, color, VerbosePkgLists, and ILoveCandy
+sed -i 's/^#Color$/Color/' /etc/pacman.conf
+sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' /etc/pacman.conf
+sed -i 's/^#ILoveCandy$/ILoveCandy/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads = 5$/ParallelDownloads = 15/' /etc/pacman.conf
+
+echo "Pacman has been configured successfully!"
+
+# Restart Pacman's package database
+pacman -Syy
