f7ce24e4664302ecb9b024a7b5a7cc88e6e55ce5
aliammar03 <93523392+aliammar03@users.noreply.github.com>
2023-05-28 19:46:55 +0500

    Update sudo.sh

--- Scripts/Install/sudo.sh
+++ Scripts/Install/sudo.sh
@@ -6,21 +6,15 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Check if the wheel group is already allowed to execute sudo
-if grep -q "^%wheel ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
-    echo "The wheel group is already allowed to execute sudo."
+# Check if the line is already uncommented in sudoers
+if grep -q "^%wheel ALL=(ALL:ALL) ALL" /etc/sudoers; then
+    echo "The line is already uncommented in the sudoers file."
     exit 0
 fi
 
-# Use visudo to modify the sudoers file
-if ! visudo -cf /etc/sudoers.d/wheel-group; then
-    echo "There was an error in the sudoers file syntax. Aborting."
-    exit 1
-fi
+# Uncomment the line in sudoers
+sudo sed -i 's/^# %wheel ALL=(ALL:ALL) ALL$/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Add the wheel group configuration to the sudoers file
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/wheel-group
-
-echo "The wheel group is now allowed to execute sudo."
+echo "The line has been successfully uncommented in the sudoers file."
 
 exit 0
