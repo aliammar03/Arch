ba185c3e7a79fcab458d6411c613e4473a7b5338
aliammar03 <93523392+aliammar03@users.noreply.github.com>
2023-05-29 00:15:07 +0500

    Update partitioncfdisk.sh

--- Scripts/Install/partitioncfdisk.sh
+++ Scripts/Install/partitioncfdisk.sh
@@ -26,64 +26,80 @@ mount_partition() {
     echo "Partition successfully mounted at $mount_point."
 }
 
-# Prompt for the disk to work with
-echo "Please enter the disk to work with (e.g., /dev/sda):"
-read disk
+# Loop until the user chooses to exit
+while true; do
+    # Prompt for the disk to work with
+    echo "Please enter the disk to work with (e.g., /dev/sda):"
+    read disk
 
-# Prompt for the action
-echo "Please select an action:"
-echo "1. Use cfdisk to partition the disk"
-echo "2. Partition and format root"
-echo "3. Mount boot"
-echo "4. Mount home"
-read -p "Enter the action number: " action
+    # Prompt for the action
+    echo "Please select an action:"
+    echo "1. Use cfdisk to partition the disk"
+    echo "2. Format and mount root"
+    echo "3. Mount boot"
+    echo "4. Mount home"
+    echo "5. Exit"
+    read -p "Enter the action number: " action
 
-if [[ $action -eq 1 ]]; then
-    # Use cfdisk to partition the disk
-    partition_disk
+    if [[ $action -eq 1 ]]; then
+        # Use cfdisk to partition the disk
+        partition_disk
 
-elif [[ $action -eq 2 ]]; then
-    # Partition and format a new partition
-    partition_disk
+    elif [[ $action -eq 2 ]]; then
+        # Prompt for the partition number
+        echo "Please enter the partition number to format (e.g., 1):"
+        read partition
 
-    # Prompt for the partition number
-    echo "Please enter the partition number to format (e.g., 1):"
-    read partition
+        format_partition
+        mount_point="/mnt/"
+        mount_partition
 
-    format_partition
-    mount_partition
+    elif [[ $action -eq 3 ]]; then
+        # Mount an existing partition to /mnt/boot
+        echo "Please enter the partition to mount (e.g., /dev/sda1):"
+        read partition
 
-elif [[ $action -eq 3 ]]; then
-    # Mount an existing partition to /mnt/boot
-    echo "Please enter the partition to mount (e.g., /dev/sda1):"
-    read partition
+        # Check if the partition exists
+        if [[ ! -e $partition ]]; then
+            echo "Error: Partition $partition does not exist."
+            continue
+        fi
 
-    # Check if the partition exists
-    if [[ ! -e $partition ]]; then
-        echo "Error: Partition $partition does not exist."
-        exit 1
+        mount_point="/mnt/boot"
+        mkdir -p $mount_point
+        mount_partition
+
+    elif [[ $action -eq 4 ]]; then
+        # Mount an existing partition to /mnt/home
+        echo "Please enter the partition to mount (e.g., /dev/sda1):"
+        read partition
+
+        # Check if the partition exists
+        if [[ ! -e $partition ]]; then
+            echo "Error: Partition $partition does not exist."
+            continue
+        fi
+        
+        
+
+        # Prompt to format the partition
+        read -p "Do you want to format the partition as ext4? (y/n): " format_choice
+        if [[ $format_choice == "y" || $format_choice == "Y" ]]; then
+            format_partition
+        fi
+        mount_point="/mnt/home"
+        mkdir -p $mount_point
+        mount_partition
+
+    elif [[ $action -eq 5 ]]; then
+        # Exit the script
+        echo "Exiting the script..."
+        break
+
+    else
+        echo "Invalid action selected."
+        continue
     fi
 
-    mount_point="/mnt/boot"
-    mkdir -p $mount_point
-    mount_partition
-
-elif [[ $action -eq 4 ]]; then
-    # Mount an existing partition to /mnt/home
-    echo "Please enter the partition to mount (e.g., /dev/sda1):"
-    read partition
-
-    # Check if the partition exists
-    if [[ ! -e $partition ]]; then
-        echo "Error: Partition $partition does not exist."
-        exit 1
-    fi
-
-    mount_point="/mnt/home"
-    mkdir -p $mount_point
-    mount_partition
-
-else
-    echo "Invalid action selected."
-    exit 1
-fi
+    echo "----------------------------------------"
+done
