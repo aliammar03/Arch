1a912ac50361d8f02e884434e5367898c41b2fa3
aliammar03 <93523392+aliammar03@users.noreply.github.com>
2023-05-29 17:52:28 +0500

    Update bootloader.sh

--- scripts/bootloader.sh
+++ scripts/bootloader.sh
@@ -9,17 +9,35 @@ fi
 # Install systemd-boot
 bootctl install
 
-# Prompt the user to enter the root partition
-read -p "Enter the root partition (e.g., /dev/sda2): " root_partition
+# Scan partitions and display them for selection
+echo "Scanning partitions..."
+partitions=()
+mapfile -t partitions < <(lsblk -nrdo NAME)
+echo "Available partitions:"
+for index in "${!partitions[@]}"; do
+    echo "$(($index + 1)). ${partitions[$index]}"
+done
+
+# Prompt the user to choose the root partition
+read -p "Enter the number of the root partition (e.g., 1): " root_partition_number
+
+# Validate the user input
+if [[ ! $root_partition_number =~ ^[0-9]+$ ]] || [[ $root_partition_number -lt 1 ]] || [[ $root_partition_number -gt ${#partitions[@]} ]]; then
+    echo "Invalid root partition number. Exiting."
+    exit 1
+fi
+
+# Get the selected root partition
+root_partition=${partitions[$(($root_partition_number - 1))]}
 
 # Check if the specified partition exists
-if [[ ! -e $root_partition ]]; then
-    echo "Specified root partition does not exist. Please try again."
+if [[ ! -e /dev/"$root_partition" ]]; then
+    echo "Specified root partition does not exist. Exiting."
     exit 1
 fi
 
 # Get the UUID of the root partition
-root_uuid=$(blkid -s UUID -o value $root_partition)
+root_uuid=$(blkid -s UUID -o value /dev/"$root_partition")
 
 # Determine the kernel package name
 kernel_package=""
@@ -48,7 +66,7 @@ timeout 3
 EOF
 
 echo "Systemd-boot has been installed successfully."
-echo "Bootloader entries have been created with root partition: $root_partition."
+echo "Bootloader entries have been created with root partition: /dev/$root_partition."
 echo "Using $kernel_package as the kernel."
 
 exit 0
