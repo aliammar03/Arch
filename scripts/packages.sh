c20efe4d82a29aa12a869a8b366d9dbc8d17958e
aliammar03 <93523392+aliammar03@users.noreply.github.com>
2023-05-29 19:55:24 +0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,7 +3,7 @@
 # Function to install packages from a file
 install_packages() {
   file="$1"
-  pacman -S --noconfirm - < "paclists/$file"
+  pacman -S --noconfirm - < "./script/paclists/$file"
 }
 
 # Function to display the menu
@@ -14,7 +14,9 @@ display_menu() {
   echo "3. Media"
   echo "4. Fonts"
   echo "5. Print"
-  echo "6. Apps"
+  echo "6. Gnome"
+  echo "7. Install from all package lists"
+  echo "8. Apps"
   echo "0. Exit"
   echo
   read -rp "Enter your choice: " choice
@@ -26,10 +28,22 @@ display_menu() {
     3) install_packages media.txt ;;
     4) install_packages fonts.txt ;;
     5) install_packages print.txt ;;
-    6) ./apps/apps.sh ;;  # Launch apps.sh script
+    6) install_packages gnome.txt ;;
+    7) install_all_packages ;;
+    8) ./apps.sh ;;  # Launch apps.sh script
     0) echo "Exiting..." ;;
     *) echo "Invalid choice. Please try again." ;;
   esac
+}
+
+# Function to install packages from all package list files
+install_all_packages() {
+  install_packages essential.txt
+  install_packages drivers.txt
+  install_packages media.txt
+  install_packages fonts.txt
+  install_packages print.txt
+  install_packages gnome.txt
 }
 
 # Display the menu and process the choice until the user chooses to exit
